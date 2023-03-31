d1076054e18b733d61d4e9962c248030cb2750da
Anantha Debnath <ananthadebnath103@gmail.com>
2023-04-01 02:35:17 +0600

    api loaded successfully

--- api.js
+++ api.js
@@ -1,3 +1,17 @@
+// function loadData(){
+//     let url = 'https://openapi.programming-hero.com/api/ai/tools';
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => console.log(data.data.tools[0].name))
+// }
+// loadData();
+
+function loadData() {
+    let url = "https://openapi.programming-hero.com/api/ai/tools";
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }
 
 
-
+  loadData();
